b60864b68d5bf002e83758aa5dcf1f98d62121c1
etrickel <erik.trickel@asu.edu>
2019-07-07 06:44:40 -0700

    adding install scrit and emacs theme

---
+++ .emacs.d/init.el
@@ -0,0 +1,184 @@
+;;84;0;0c
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4d1b5369d16b9e9abbf7fbcad511ac1b539010567fb1ed217f78cc638fec1f99" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'monokai)
+
+;; (require 'ido)
+;; (ido-mode t)
+
+(electric-indent-mode 0)
+
+;; (require 'package) ;; You might already have this line
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "https://melpa.org/packages/"))
+;; (when (< emacs-major-version 24)
+;;   ;; For important compatibility libraries like cl-lib
+;;   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; (package-initialize) ;; You might already have this line
+
+;; (require 'smex) ; Not needed if you use package.el
+;; (smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+;;                     ; when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)	
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  ;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(ac-config-default)
+
+(setq linum-format "%d ")
+(global-linum-mode 1)
+
+(show-paren-mode 1)
+
+(setq c-default-style "k&r"
+          c-basic-offset 4)
+
+(setq backup-directory-alist
+      `((".*" . ,"~/tmp/emacs-backups/")))
+(setq auto-save-file-name-transforms
+      `((".*" ,"~/tmp/emacs-saves/" t)))
+
+;;(xterm-mouse-mode)
+
+(setq x-select-enable-clipboard t)
+
+(defun volatile-kill-buffer ()
+   "Kill current buffer unconditionally."
+   (interactive)
+   (let ((buffer-modified-p nil))
+     (kill-buffer (current-buffer))))
+
+
+(global-set-key (kbd "C-x k") 'server-edit)   
+
+;;(global-set-key (kbd "C-x k") 'volatile-kill-buffer)   
+
+;;(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+
+
+(defun copy-all ()
+    "Copy entire buffer to clipboard"
+    (interactive)
+    (clipboard-kill-ring-save (point-min) (point-max)))
+
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+
+(global-set-key (kbd "<f8>") 'ispell-word)
+(defun flyspell-check-next-highlighted-word ()
+  "Custom function to spell check next highlighted word"
+  (interactive)
+  (flyspell-goto-next-error)
+  (ispell-word))
+(global-set-key (kbd "C-<f8>") 'flyspell-check-next-highlighted-word)
+
+;; handle tmux's xterm-keys
+;; put the following line in your ~/.tmux.conf:
+;;   setw -g xterm-keys on
+(if (getenv "TMUX")
+    (progn
+      (let ((x 2) (tkey ""))
+    (while (<= x 8)
+      ;; shift
+      (if (= x 2)
+          (setq tkey "S-"))
+      ;; alt
+      (if (= x 3)
+          (setq tkey "M-"))
+      ;; alt + shift
+      (if (= x 4)
+          (setq tkey "M-S-"))
+      ;; ctrl
+      (if (= x 5)
+          (setq tkey "C-"))
+      ;; ctrl + shift
+      (if (= x 6)
+          (setq tkey "C-S-"))
+      ;; ctrl + alt
+      (if (= x 7)
+          (setq tkey "C-M-"))
+      ;; ctrl + alt + shift
+      (if (= x 8)
+          (setq tkey "C-M-S-"))
+
+      ;; arrows
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d A" x)) (kbd (format "%s<up>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d B" x)) (kbd (format "%s<down>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d C" x)) (kbd (format "%s<right>" tkey)))
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d D" x)) (kbd (format "%s<left>" tkey)))
+      ;; home
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d H" x)) (kbd (format "%s<home>" tkey)))
+      ;; end
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d F" x)) (kbd (format "%s<end>" tkey)))
+      ;; page up
+      (define-key key-translation-map (kbd (format "M-[ 5 ; %d ~" x)) (kbd (format "%s<prior>" tkey)))
+      ;; page down
+      (define-key key-translation-map (kbd (format "M-[ 6 ; %d ~" x)) (kbd (format "%s<next>" tkey)))
+      ;; insert
+      (define-key key-translation-map (kbd (format "M-[ 2 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+      ;; delete
+      (define-key key-translation-map (kbd (format "M-[ 3 ; %d ~" x)) (kbd (format "%s<delete>" tkey)))
+      ;; f1
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d P" x)) (kbd (format "%s<f1>" tkey)))
+      ;; f2
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d Q" x)) (kbd (format "%s<f2>" tkey)))
+      ;; f3
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d R" x)) (kbd (format "%s<f3>" tkey)))
+      ;; f4
+      (define-key key-translation-map (kbd (format "M-[ 1 ; %d S" x)) (kbd (format "%s<f4>" tkey)))
+      ;; f5
+      (define-key key-translation-map (kbd (format "M-[ 15 ; %d ~" x)) (kbd (format "%s<f5>" tkey)))
+      ;; f6
+      (define-key key-translation-map (kbd (format "M-[ 17 ; %d ~" x)) (kbd (format "%s<f6>" tkey)))
+      ;; f7
+      (define-key key-translation-map (kbd (format "M-[ 18 ; %d ~" x)) (kbd (format "%s<f7>" tkey)))
+      ;; f8
+      (define-key key-translation-map (kbd (format "M-[ 19 ; %d ~" x)) (kbd (format "%s<f8>" tkey)))
+      ;; f9
+      (define-key key-translation-map (kbd (format "M-[ 20 ; %d ~" x)) (kbd (format "%s<f9>" tkey)))
+      ;; f10
+      (define-key key-translation-map (kbd (format "M-[ 21 ; %d ~" x)) (kbd (format "%s<f10>" tkey)))
+      ;; f11
+      (define-key key-translation-map (kbd (format "M-[ 23 ; %d ~" x)) (kbd (format "%s<f11>" tkey)))
+      ;; f12
+      (define-key key-translation-map (kbd (format "M-[ 24 ; %d ~" x)) (kbd (format "%s<f12>" tkey)))
+      ;; f13
+      (define-key key-translation-map (kbd (format "M-[ 25 ; %d ~" x)) (kbd (format "%s<f13>" tkey)))
+      ;; f14
+      (define-key key-translation-map (kbd (format "M-[ 26 ; %d ~" x)) (kbd (format "%s<f14>" tkey)))
+      ;; f15
+      (define-key key-translation-map (kbd (format "M-[ 28 ; %d ~" x)) (kbd (format "%s<f15>" tkey)))
+      ;; f16
+      (define-key key-translation-map (kbd (format "M-[ 29 ; %d ~" x)) (kbd (format "%s<f16>" tkey)))
+      ;; f17
+      (define-key key-translation-map (kbd (format "M-[ 31 ; %d ~" x)) (kbd (format "%s<f17>" tkey)))
+      ;; f18
+      (define-key key-translation-map (kbd (format "M-[ 32 ; %d ~" x)) (kbd (format "%s<f18>" tkey)))
+      ;; f19
+      (define-key key-translation-map (kbd (format "M-[ 33 ; %d ~" x)) (kbd (format "%s<f19>" tkey)))
+      ;; f20
+      (define-key key-translation-map (kbd (format "M-[ 34 ; %d ~" x)) (kbd (format "%s<f20>" tkey)))
+
+      (setq x (+ x 1))
+      ))
+    )
+   )     
+
+
+
+
