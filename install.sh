5307782094a2c1f41a95b03f6e13e3bf85a0937f
etrickel <erik.trickel@asu.edu>
2019-07-07 06:53:15 -0700

    wrong url for github

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 !#/usr/bin/env bash
 
-wget https://github.com/etrickel/docker_env/blob/master/.bash_prompt -O ~/.bash_prompt
-wget https://github.com/etrickel/docker_env/blob/master/.bashrc -O ~/.bashrc
-wget https://github.com/etrickel/docker_env/blob/master/.inputrc -O ~/.inputrc
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/.bash_prompt -O ~/.bash_prompt
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/.bashrc -O ~/.bashrc
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/.inputrc -O ~/.inputrc
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
 fi 
 
-wget https://github.com/etrickel/docker_env/blob/master/authorized_keys -O- >> ~/.ssh/authorized_keys
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/authorized_keys -O- >> ~/.ssh/authorized_keys
 
 if [ ! -d ~/.emacs.d ]; then
   mkdir ~/.emacs.d
   mkdir ~/.emacs.d/themes
 fi 
 
-wget https://github.com/etrickel/docker_env/blob/master/.emacs.d/init.el -O ~/.emacs.d/init.el
-wget https://github.com/etrickel/docker_env/blob/master/.emacs.d/themes/monokai-theme.el -O ~/.emacs.d/themes/monokai-theme.el
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/init.el -O ~/.emacs.d/init.el
+wget https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/themes/monokai-theme.el -O ~/.emacs.d/themes/monokai-theme.el
 
 
 
