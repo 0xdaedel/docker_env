ad0ae6f3f0553a601cbc0eba2faa84fa9463fb51
etrickel <erik.trickel@asu.edu>
2019-07-07 06:55:32 -0700

    changed to curl

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 !#/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/.bash_prompt -O ~/.bash_prompt
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/.bashrc -O ~/.bashrc
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/.inputrc -O ~/.inputrc
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.bash_prompt -O ~/.bash_prompt
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.bashrc > ~/.bashrc
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.inputrc > ~/.inputrc
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
 fi 
 
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/authorized_keys -O- >> ~/.ssh/authorized_keys
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/authorized_keys >> ~/.ssh/authorized_keys
 
 if [ ! -d ~/.emacs.d ]; then
   mkdir ~/.emacs.d
   mkdir ~/.emacs.d/themes
 fi 
 
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/init.el -O ~/.emacs.d/init.el
-wget https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/themes/monokai-theme.el -O ~/.emacs.d/themes/monokai-theme.el
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/init.el > ~/.emacs.d/init.el
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.emacs.d/themes/monokai-theme.el > ~/.emacs.d/themes/monokai-theme.el
 
 
 
