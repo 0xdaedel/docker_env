43c6ec2bff414ea760ff8d468851d76df242ffe4
etrickel <erik.trickel@asu.edu>
2019-07-07 07:03:10 -0700

    wip

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.bash_prompt -O ~/.bash_prompt
+
 curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.bashrc > ~/.bashrc
+curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.bash_prompt > ~/.bash_prompt
 curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/.inputrc > ~/.inputrc
+
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
 fi 
-
 curl -sL https://raw.githubusercontent.com/etrickel/docker_env/master/authorized_keys >> ~/.ssh/authorized_keys
 
 if [ ! -d ~/.emacs.d/themes ]; then
